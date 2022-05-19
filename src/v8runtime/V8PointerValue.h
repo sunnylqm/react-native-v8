6dc25f2cd79d51de353e42386d0a23e9c32cc3d5
Kudo Chien <ckchien@gmail.com>
2022-05-19 20:33:47 +0800

    Fix crash for accessing V8::Global dangling pointer (#107)
    when `V8::Global::Reset` in V8PointerValue dtor, we should also protect by a lock. otherwise, there're chances to access dangling pointers.
    
    that should fix the problem @jzxchiang1 had in https://github.com/software-mansion/react-native-reanimated/pull/3132#issuecomment-1112878884
    # Conflicts:
    #	src/v8runtime/V8PointerValue.cpp
    #	src/v8runtime/V8PointerValue.h

--- src/v8runtime/V8PointerValue.h
+++ src/v8runtime/V8PointerValue.h
@@ -9,7 +9,7 @@ namespace facebook {
 class V8PointerValue final : public V8Runtime::PointerValue {
  public:
   V8PointerValue(v8::Isolate *isolate, const v8::Local<v8::Value> &value);
-  ~V8PointerValue();
+  ~V8PointerValue() override;
 
   v8::Local<v8::Value> Get(v8::Isolate *isolate) const;
 
@@ -26,6 +26,7 @@ class V8PointerValue final : public V8Runtime::PointerValue {
  private:
   friend class JSIV8ValueConverter;
   friend class V8Runtime;
+  v8::Isolate *isolate_;
   v8::Global<v8::Value> value_;
 };
 
